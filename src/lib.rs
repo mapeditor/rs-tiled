15af3b58fe8cb6c1985494e9fa64731c65ddc6d2
Frizi <frizi09@gmail.com>
2018-12-17 18:12:27 +0100

    Add tile type and probability parsing

--- src/lib.rs
+++ src/lib.rs
@@ -441,23 +441,28 @@ pub struct Tile {
     pub properties: Properties,
     pub objectgroup: Option<ObjectGroup>,
     pub animation: Option<Vec<Frame>>,
+    pub tile_type: Option<String>,
+    pub probability: f32,
 }
 
 const FLIPPED_HORIZONTALLY_FLAG: u32 = 0x8;
 const FLIPPED_VERTICALLY_FLAG: u32 = 0x4;
 const FLIPPED_DIAGONALLY_FLAG: u32 = 0x2;
-const ALL_FLIP_FLAGS: u32 = 0xE0000000;
+const ALL_FLIP_FLAGS: u32 = FLIPPED_HORIZONTALLY_FLAG | FLIPPED_VERTICALLY_FLAG | FLIPPED_DIAGONALLY_FLAG;
 
 impl Tile {
     fn new<R: Read>(parser: &mut EventReader<R>, attrs: Vec<OwnedAttribute>) -> Result<Tile, TiledError> {
-        let (_, i): (_, u32) = get_attrs!(
+        let ((tile_type, probability), id) = get_attrs!(
             attrs,
-            optionals: [],
-            required: [("id", id, |v:String| v.parse().ok())],
+            optionals: [
+                ("type", tile_type, |v:String| v.parse().ok()),
+                ("probability", probability, |v:String| v.parse().ok())
+            ],
+            required: [("id", id, |v:String| v.parse::<u32>().ok())],
             TiledError::MalformedAttributes("tile must have an id with the correct type".to_string()));
         
-        let flags = (i & ALL_FLIP_FLAGS) >> 28;
-        let i: u32 = i & 0x1FFFFFFF;
+        let flags = (id & ALL_FLIP_FLAGS) >> 28;
+        let id: u32 = id & !ALL_FLIP_FLAGS;
         let diagon = flags & FLIPPED_DIAGONALLY_FLAG == FLIPPED_DIAGONALLY_FLAG;
         let flip_h = (flags & FLIPPED_HORIZONTALLY_FLAG == FLIPPED_HORIZONTALLY_FLAG) ^ diagon;
         let flip_v = (flags & FLIPPED_VERTICALLY_FLAG == FLIPPED_VERTICALLY_FLAG) ^ diagon;
@@ -483,7 +488,7 @@ impl Tile {
                        animation = Some(parse_animation(parser)?);
                        Ok(())
                    });
-        Ok(Tile {id: i, flip_h, flip_v, images: images, properties: properties, objectgroup: objectgroup, animation: animation})
+        Ok(Tile {id, flip_h, flip_v, images, properties, objectgroup, animation, tile_type, probability: probability.unwrap_or(1.0)})
     }
 }
 
