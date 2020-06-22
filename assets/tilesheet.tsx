bab5d2f35cb7b9e2dcb3cd51e082a467127dbb9e
tatref <tatref@github.com>
2020-06-22 23:49:07 +0200

    Update test maps to Tiled 1.4

--- assets/tilesheet.tsx
+++ assets/tilesheet.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tilesheet" tilewidth="32" tileheight="32">
+<tileset version="1.4" tiledversion="1.4.0" name="tilesheet" tilewidth="32" tileheight="32" tilecount="84" columns="14">
  <properties>
   <property name="tileset property" value="tsp"/>
  </properties>
