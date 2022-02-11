187f1fbe32bd7b6b2f45ab410cc084a0e1a4bfa3
Alejandro Perea <alexpro820@gmail.com>
2022-02-11 09:31:34 +0100

    Add SFML example (#133)
    * Add SFML example
    Adds a rough demo using SFML for rendering.
    
    * Simplifications to SFML example
    
    * Rely on sfml 0.16 rather than a git revision
    * Avoid allocating temporary Path
    * Take into account margin and spacing
    * Directly produce a FloatRect, since it is what SFML needs
    * Renamed VertexMesh to QuadMesh
    * Use Vertex::with_pos_coords since we always passed Color::WHITE
    * Removed TileLayer wrapper, since it added no value
    * Removed handling of gid == 0 in Tilesheet::tile_rect
    
    * Update contributors list
    
    * Apply formatting
    
    * Avoid the need to inverse the camera matrix
    
    * De-hardcode the tile size
    
    Also fixes the example a bit, because the map was drawn too small.
    
    * Update workflow
    
    * Try fixing CI issue by removing rust-cache
    
    * Fix CI
    
    * Fix map asset path
    
    * Merge SFML and Rust workflow
    
    * Misc changes
    
    * Merge upstream/master
    
    * Update example to master
    
    * Fix line endings
    
    * Fixed comment
    
    Co-authored-by: Thorbjørn Lindeijer <bjorn@lindeijer.nl>

---
+++ examples/sfml/mesh.rs
@@ -0,0 +1,43 @@
+use sfml::{
+    graphics::{Drawable, FloatRect, PrimitiveType, Vertex},
+    system::Vector2f,
+};
+
+pub struct QuadMesh(Vec<Vertex>);
+
+impl QuadMesh {
+    /// Create a new mesh with capacity for the given amount of quads.
+    pub fn with_capacity(quads: usize) -> Self {
+        Self(Vec::with_capacity(quads * 4))
+    }
+
+    /// Add a quad made up of vertices to the mesh.
+    pub fn add_quad(&mut self, position: Vector2f, size: f32, uv: FloatRect) {
+        self.0.push(Vertex::with_pos_coords(
+            position,
+            Vector2f::new(uv.left, uv.top),
+        ));
+        self.0.push(Vertex::with_pos_coords(
+            position + Vector2f::new(size, 0f32),
+            Vector2f::new(uv.left + uv.width, uv.top),
+        ));
+        self.0.push(Vertex::with_pos_coords(
+            position + Vector2f::new(size, size),
+            Vector2f::new(uv.left + uv.width, uv.top + uv.height),
+        ));
+        self.0.push(Vertex::with_pos_coords(
+            position + Vector2f::new(0f32, size),
+            Vector2f::new(uv.left, uv.top + uv.height),
+        ));
+    }
+}
+
+impl Drawable for QuadMesh {
+    fn draw<'a: 'shader, 'texture, 'shader, 'shader_texture>(
+        &'a self,
+        target: &mut dyn sfml::graphics::RenderTarget,
+        states: &sfml::graphics::RenderStates<'texture, 'shader, 'shader_texture>,
+    ) {
+        target.draw_primitives(&self.0, PrimitiveType::QUADS, states);
+    }
+}
